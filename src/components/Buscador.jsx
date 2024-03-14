f864c27dea15f08f738eeee737bc8d1a5630142e
Ezequiel Nicolas Viera <110434542+S-Kiev@users.noreply.github.com>
2024-03-14 16:11:49 -0300

    Add files via upload

---
+++ src/components/Buscador.jsx
@@ -0,0 +1,49 @@
+/*
+import { useState, useEffect } from "react"
+import productos from '../JSON_Productos/productos.json'
+import './buscador.css'
+
+const Buscador = ({ handleSearch, scrollTablaVentas }) => {
+  const [resultado, setResultado] = useState([])
+  const [busqueda, setBusqueda] = useState('')
+
+  const handleFilter = (e) => {
+    const valorBusqueda = e.target.value.toLocaleLowerCase()
+    setResultado(
+      productos.filter(
+        (p) => JSON.stringify(p).toLocaleLowerCase().includes(valorBusqueda)
+      )
+    )
+    setBusqueda(valorBusqueda)
+    handleSearch(valorBusqueda)
+  }
+
+  const handleReset = () => {
+    setBusqueda('')
+    handleFilter({target: {value: ''}})
+  }
+
+  useEffect(() => {
+    if (productos) setResultado(productos)
+  }, [productos])
+
+  const handleTabla = () => {
+    setTimeout(() => {
+      scrollTablaVentas();
+    }, 100);
+  };
+
+  return (
+    <nav className="buscador">
+      <div className="input-container">
+        <input type="text" placeholder="Buscador" value={busqueda} onChange={handleFilter} />
+        <button onClick={handleReset}>Resetear</button>
+      </div>
+    </nav>
+  )
+}
+
+export default Buscador
+
+  
+*/
